1eb06956c169262beb9c2245cb0cb4edab0e6821
technoborsch <75335319+technoborsch@users.noreply.github.com>
2021-05-11 18:10:38 +0300

    The models and their tests from previous project were adapted for this new one

--- AtomproektBase/models.py
+++ AtomproektBase/models.py
@@ -1,3 +1,114 @@
 from django.db import models
+from pytils.translit import slugify
 
-# Create your models here.
+
+class SlugBase(models.Model):
+    """Base to add a slug field to a model"""
+
+    field_to_slugify = ''
+
+    slug = models.SlugField(
+        allow_unicode=True,
+        blank=True,
+        null=True,
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            if not self.field_to_slugify:
+                raise ValueError('Specify a field to slugify')
+            elif not hasattr(self, self.field_to_slugify):
+                raise ValueError('Wrong field to slugify - here is no such field')
+            self.slug = slugify(self.__getattribute__(self.field_to_slugify))
+        super(SlugBase, self).save(*args, **kwargs)
+
+    class Meta:
+        abstract = True
+
+
+class Project(SlugBase):
+    """Model for a project of NPP"""
+
+    name = models.CharField(max_length=30, unique=True, verbose_name='Имя проекта')
+    country = models.CharField(max_length=30, verbose_name='Страна')
+    description = models.TextField(max_length=1000, verbose_name='Описание')
+    stage = models.CharField(max_length=10, verbose_name='Текущая стадия')
+
+    field_to_slugify = 'name'
+
+
+class Building(SlugBase):
+    """Model for a building of NPP"""
+
+    kks = models.CharField(
+        max_length=20,
+        verbose_name='KKS-код здания'
+    )
+    name = models.CharField(max_length=200, verbose_name='Официальное точное название')
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name='buildings',
+        verbose_name='Проект, которому принадлежит здание',
+    )
+
+    field_to_slugify = 'kks'
+
+    class Meta:
+        ordering = ['kks']
+        constraints = [
+            models.UniqueConstraint(
+                fields=['kks', 'project'],
+                name='Здание с таким KKS-кодом уже присутсвует в данном проекте',
+            )  # To raise validation error when trying to add an existing building to a project
+        ]
+
+
+class System(SlugBase):
+    """The model for a system - part of the NPP"""
+
+    SEISMIC_CATEGORIES = [
+        ("1", "First"),
+        ("2", "Second"),
+        ("3", "Third"),
+        ("4", "Fourth"),
+        ("NA", "Not specified"),
+    ]
+
+    SAFETY_CATEGORIES = [
+        ("1", "First class"),
+        ("2", "Second class"),
+        ("3", "Third class"),
+        ("4", "Fourth class"),
+        ("EYT", "EYT"),
+        ("NA", "Not specified"),
+    ]
+
+    kks = models.CharField(
+        max_length=20,
+        unique=True,
+        verbose_name='KKS-код системы'
+    )
+    name = models.CharField(max_length=200, verbose_name='Точное название системы')
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name='systems',
+        verbose_name='Проект, которому принадлежит система')
+    buildings = models.ManyToManyField(
+        Building,
+        related_name='systems',
+        verbose_name='Перечень зданий, в которых находится система')
+    seismic_category = models.CharField(max_length=3, choices=SEISMIC_CATEGORIES)
+    safety_category = models.CharField(max_length=3, choices=SAFETY_CATEGORIES)
+
+    field_to_slugify = 'kks'
+
+    class Meta:
+        ordering = ['kks']
+        constraints = [
+            models.UniqueConstraint(
+                fields=['kks', 'project'],
+                name='Система с таким KKS-кодом уже присутсвует в данном проекте'
+            )  # To raise validation error when trying to add an existing building to a project
+        ]
---
+++ AtomproektBase/test/test_models.py
@@ -0,0 +1,112 @@
+from django.test import TestCase
+from AtomproektBase import models
+
+
+class SetUp(TestCase):
+    """Base setup for all test cases"""
+    def setUp(self) -> None:
+        self.project1 = models.Project.objects.create(
+            name="Суперстанция-1",
+            country="Utopia",
+            description="A fancy NPP",
+            stage="forever")
+        self.project2 = models.Project.objects.create(
+            name="Сверхстанция-2",
+            country="Narnia",
+            description="The fanciest NPP",
+            stage="ever")
+        self.building1_1 = models.Building.objects.create(
+            kks='10UJA',
+            name='Reactor building',
+            project=self.project1)
+        self.building1_2 = models.Building.objects.create(
+            kks='10UKA',
+            name='Auxiliary building',
+            project=self.project1)
+        self.system1_1 = models.System.objects.create(
+            kks='10JRT',
+            name='Have_no_idea',
+            project=self.project1,
+            seismic_category='1',
+            safety_category='1')
+        self.system1_1.buildings.add(self.building1_1)
+        self.system1_2 = models.System.objects.create(
+            kks='10KAA',
+            name='Cooler',
+            project=self.project1,
+            seismic_category='1',
+            safety_category='2')
+        self.system1_2.buildings.add(self.building1_1)
+        self.system1_2.buildings.add(self.building1_2)
+
+        for item in self.__dict__:  # just full-clean all self-models.
+            if hasattr(item, 'full_clean'):
+                item.full_clean()
+
+
+class SlugBaseTest(SetUp):
+    """Tests for a base that adds slug field"""
+    def testRaises(self):
+        """Tests if it raises errors as expected"""
+        cases = ['', 'field_that_doesnt_exist']  # should trigger error
+        for case in cases:
+            with self.subTest(msg=case):
+                with self.assertRaises(ValueError):
+                    test_model_class = type(
+                        'TestModel' + case, (models.SlugBase,),
+                        {
+                            'field_to_slugify': case,
+                            '__module__': __name__,
+                        })
+                    test_obj = test_model_class()
+                    test_obj.save()
+
+
+class ProjectTest(SetUp):
+    """Tests for project model"""
+
+    def test_slug(self):
+        """Checks if slug for model is correct"""
+        cases = [
+            (self.project1, "superstantsiya-1"),
+            (self.project2, "sverhstantsiya-2"),
+        ]
+        for project, slug in cases:
+            with self.subTest(msg=str(project)):
+                self.assertEqual(project.slug, slug)
+
+    def test_order(self):
+        """Checks correct ordering of objects"""
+
+        self.assertEqual(
+            list(models.Project.objects.all()),
+            [self.project1, self.project2]
+        )
+
+
+class BuildingTest(SetUp):
+    """Tests for building model"""
+
+    def test_slug(self):
+        """Checks if slug for model is correct"""
+        cases = [
+            (self.building1_1, "10uja"),
+            (self.building1_2, "10uka"),
+        ]
+        for project, slug in cases:
+            with self.subTest(msg=str(project)):
+                self.assertEqual(project.slug, slug)
+
+
+class SystemTest(SetUp):
+    """Tests for system model"""
+
+    def test_slug(self):
+        """Checks if slug for model is correct"""
+        cases = [
+            (self.system1_1, "10jrt"),
+            (self.system1_2, "10kaa"),
+        ]
+        for project, slug in cases:
+            with self.subTest(msg=str(project)):
+                self.assertEqual(project.slug, slug)
