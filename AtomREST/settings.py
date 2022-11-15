7e8aef01734f29f2e91b5f4cf45275a065e86401
technoborsch <75335319+technoborsch@users.noreply.github.com>
2022-11-15 14:53:50 +0300

    Some additional settings to maybe finally be able to authenticate

--- AtomREST/settings.py
+++ AtomREST/settings.py
@@ -166,6 +166,8 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
 ]
 
+CORS_ALLOW_CREDENTIALS = True
+
 CSRF_TRUSTED_ORIGINS = [
     'https://localhost:3000',
     'https://127.0.0.1:8000',
