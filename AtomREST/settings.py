9bd7b2786db736cd7ff590b95997885f78ff6399
technoborsch <75335319+technoborsch@users.noreply.github.com>
2022-11-15 12:26:38 +0300

    Basic authentication added

--- AtomREST/settings.py
+++ AtomREST/settings.py
@@ -183,6 +183,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'knox.auth.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     )
 }
 
